20c25ec13c141463e2238c067442034cfc666522
CKentarou <k023c0018@g.neec.ac.jp>
2025-03-19 15:49:11 +0900

    Initial Commit

---
+++ src/main/java/com/example/AjiNavi/AjiNaviApplication.java
@@ -0,0 +1,13 @@
+package com.example.AjiNavi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjiNaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjiNaviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AjiNavi/AjiNaviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AjiNavi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjiNaviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
