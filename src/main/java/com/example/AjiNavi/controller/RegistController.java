95cd8f88e0f49b70af7c50d5fbbff13b8cad39da
CKentarou <k023c0018@g.neec.ac.jp>
2025-03-20 21:13:33 +0900

    入力フォームにバリデーションを追加

--- src/main/java/com/example/AjiNavi/controller/RegistController.java
+++ src/main/java/com/example/AjiNavi/controller/RegistController.java
@@ -3,6 +3,8 @@ package com.example.AjiNavi.controller;
 import com.example.AjiNavi.form.ReviewForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,9 +18,14 @@ public class RegistController {
     }
 
     @PostMapping("/confirm-regist-review")
-    public String confirmRegistReview(@ModelAttribute ReviewForm form,Model model) {
+    public String confirmRegistReview(@Validated @ModelAttribute ReviewForm form, BindingResult result, Model model) {
+
+        if (result.hasErrors()) {
+            return "regist-review";
+        }
+
         model.addAttribute("msg", "登録されました");
-        System.out.println(form);
         return "complete-regist-review";
     }
+
 }
--- src/main/java/com/example/AjiNavi/form/ReviewForm.java
+++ src/main/java/com/example/AjiNavi/form/ReviewForm.java
@@ -1,13 +1,26 @@
 package com.example.AjiNavi.form;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import java.sql.Date;
 
 @Data
 public class ReviewForm {
+    @NotNull(message="入力して下さい")
+    @Min(value=1, message="1以上の数字を入力して下さい")
     private Integer restaurantId;
+
+    @Size(min=4, max=16, message="4文字から16文字で指定してください。")
     private String userId;
+
+    @Past(message="今日以前の日付を入力してください。")
     private Date visitDate;
+
+    @NotNull(message="入力してください。")
+    @Min(value=1, message="1-5で指定してください。")
+    @Max(value=5, message="1-5で指定してください。")
     private Integer rating;
+
+    @Size(min=1, max=128, message="1文字から128文字で指定してください。")
     private String comment;
 }
