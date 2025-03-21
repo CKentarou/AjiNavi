b3af59969ecaca68254a3ca54973cf0f345a4d75
CKentarou <k023c0018@g.neec.ac.jp>
2025-03-21 10:46:14 +0900

    データベースへの登録処理を追加

--- src/main/java/com/example/AjiNavi/repository/ReviewRepositoryImpl.java
+++ src/main/java/com/example/AjiNavi/repository/ReviewRepositoryImpl.java
@@ -1,12 +1,27 @@
 package com.example.AjiNavi.repository;
 
 import com.example.AjiNavi.entity.Review;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Repository
+@RequiredArgsConstructor
 public class ReviewRepositoryImpl implements ReviewRepository {
+
+    private final JdbcTemplate jdbcTemplete;
+
     @Override
     public void add(Review review) {
-        System.out.println(review);
+        String sql = "INSERT INTO t_review (restaurant_id, user_id, visit_date, rating, comment) VALUES (?, ?, ?, ?, ?)";
+
+        jdbcTemplete.update(
+                sql,
+                review.getRestaurantId(),
+                review.getUserId(),
+                review.getVisitDate(),
+                review.getRating(),
+                review.getComment()
+        );
     }
 }
