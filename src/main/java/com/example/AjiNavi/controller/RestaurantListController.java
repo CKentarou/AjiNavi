4760811f43878cfadb439664924799d75476d138
CKentarou <k023c0018@g.neec.ac.jp>
2025-04-02 14:47:32 +0900

    店舗検索機能の実装

--- src/main/java/com/example/AjiNavi/controller/RestaurantListController.java
+++ src/main/java/com/example/AjiNavi/controller/RestaurantListController.java
@@ -2,6 +2,8 @@ package com.example.AjiNavi.controller;
 
 import com.example.AjiNavi.entity.Restaurant;
 import com.example.AjiNavi.form.RestaurantSearchForm;
+import com.example.AjiNavi.service.RestaurantListService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +14,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class RestaurantListController {
+    //ここで使用しているRestaurantListServiceはインターフェース型である。業務処理の実装クラスがなくても定義自体はできる。
+    private final RestaurantListService service;
+
     @GetMapping("/top")
     private String restaurantList(@ModelAttribute RestaurantSearchForm form) {
         return "restaurant-list";
@@ -20,11 +26,9 @@ public class RestaurantListController {
 
     @PostMapping("/restaurant-search")
     private String restaurantSearch(@ModelAttribute RestaurantSearchForm form, Model model) {
-        List<Restaurant> list = new ArrayList<Restaurant>();
-        list.add(new Restaurant(1, "店舗１", "キャッチフレーズ", 3.5));
-        list.add(new Restaurant(2, "店舗2", "キャッチフレーズ", 0.0));
-        list.add(new Restaurant(3, "店舗3", "キャッチフレーズ", 4.2));
 
+        //formには入力された値が入っている。ゲッターを使って引数に渡す。
+        List<Restaurant> list = service.findByNameWildcard(form.getRestaurantName());
         model.addAttribute("restaurantList", list);
 
         return "restaurant-list";
