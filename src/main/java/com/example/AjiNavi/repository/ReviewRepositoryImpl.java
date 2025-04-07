4f08e4ecba2a8877fb4135421a7d147f7c592e96
CKentarou <k023c0018@g.neec.ac.jp>
2025-04-07 13:32:51 +0900

    Review一覧取得用のメソッド追加

--- src/main/java/com/example/AjiNavi/repository/ReviewRepositoryImpl.java
+++ src/main/java/com/example/AjiNavi/repository/ReviewRepositoryImpl.java
@@ -2,8 +2,14 @@ package com.example.AjiNavi.repository;
 
 import com.example.AjiNavi.entity.Review;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.relational.core.sql.In;
 import org.springframework.stereotype.Repository;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 @RequiredArgsConstructor
@@ -23,5 +29,24 @@ public class ReviewRepositoryImpl implements ReviewRepository {
                 review.getRating(),
                 review.getComment()
         );
+    }
+
+    @Override
+    public List<Review> selectByRestaurantId(int restaurantId) {
+        String sql = "SELECT review_id, restaurant_id, user_id, visit_date, rating, comment From t_review Where restaurant_id = ? ORDER BY visit_date DESC, review_id ASC";
+
+        List<Map<String, Object>> list = jdbcTemplete.queryForList(sql, restaurantId);
+        List<Review> result = new ArrayList<Review>();
+        for (Map<String, Object> one : list) {
+            Review review = new Review();
+            review.setReviewId((Integer) one.get("review_id"));
+            review.setRestaurantId((Integer) one.get("restaurant_id"));
+            review.setUserId((String) one.get("user_id"));
+            review.setVisitDate((Date)one.get("visit_date"));
+            review.setRating((Integer) one.get("rating"));
+            review.setComment((String) one.get("comment"));
+            result.add(review);
+        }
+        return result;
     }
 }
